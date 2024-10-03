6c142c3ca4b97716a19af7687c672d3712bd0718
MikulDev <gamer3mc@gmail.com>
2024-10-03 15:29:41 -0400

    Add multiplayer screen

--- MainMenu.cs
+++ MainMenu.cs
@@ -3,10 +3,16 @@ using System;
 
 public partial class MainMenu : Control
 {
-	public const string SINGLEPLAYER_PATH = "res://world/game_world.tscn";
+	public const string GAME_PATH = "res://world/game_world.tscn";
+	public const string MULTIPLAYER_SCREEN_PATH = "res://menu/multiplayer_menu.tscn";
 	
 	public void singleplayerPressed()
 	{
-		GetTree().ChangeSceneToFile(SINGLEPLAYER_PATH);
+		GetTree().ChangeSceneToFile(GAME_PATH);
+	}
+	
+	public void multiplayerPressed()
+	{
+		GetTree().ChangeSceneToFile(MULTIPLAYER_SCREEN_PATH);
 	}
 }
